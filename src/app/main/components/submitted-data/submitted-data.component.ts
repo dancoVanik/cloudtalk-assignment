53db52048f7df48c03d835033b0e552eb3affe0c
Daniel Vaník <vanik@netgrif.com>
2021-03-05 23:25:38 +0100

    - send and set form data to other component

--- src/app/main/components/submitted-data/submitted-data.component.ts
+++ src/app/main/components/submitted-data/submitted-data.component.ts
@@ -5,14 +5,18 @@ import {UserDataService} from '../../services/user-data.service';
 @Component({
     selector: 'app-submitted-data',
     templateUrl: './submitted-data.component.html',
-    styleUrls: ['./submitted-data.component.scss'],
-    providers: [UserDataService]
+    styleUrls: ['./submitted-data.component.scss']
 })
 export class SubmittedDataComponent implements OnInit {
 
     public submittedDataFormGroupControl: FormGroup
 
     constructor(private _userDataService: UserDataService) {
+        this._userDataService.userDataFormGroupControl$.subscribe(groupControl => {
+            this.submittedDataFormGroupControl.get('nameControl').setValue(groupControl.get('nameControl').value)
+            this.submittedDataFormGroupControl.get('emailControl').setValue(groupControl.get('emailControl').value)
+            this.submittedDataFormGroupControl.get('phoneControl').setValue(groupControl.get('phoneControl').value)
+        });
     }
 
     ngOnInit(): void {
@@ -22,10 +26,6 @@ export class SubmittedDataComponent implements OnInit {
             phoneControl: new FormControl(null),
         });
         this.submittedDataFormGroupControl.disable()
-
-        this._userDataService.userDataFormGroupControl$.subscribe(groupControl => {
-            this.submittedDataFormGroupControl = groupControl
-        });
     }
 
 }
